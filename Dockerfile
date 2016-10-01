470157e56d531bb47f6b2794409c2511ab4a5629
Robert O'Farrell <robert.ofarrell+github@gmail.com>
2016-10-01 18:59:24 +1000

    Tweaks to date display

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM ruby:2.2
 
 RUN apt-get update
 RUN apt-get install -y nodejs
-
 WORKDIR /tmp
 ADD Gemfile Gemfile.lock ./
 RUN bundle install --jobs 8
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,7 @@
 //= require turbolinks
 //= require_tree .
 //
+//= require bootstrap-sprockets
+//= require bootstrap
 //= require moment
 //= require bootstrap-datetimepicker
