f83b7e73520fb208d8b2db77db7afc4a0cc46a6c
odd13 <odd13@odd13.com>
2016-12-09 12:51:22 +1100

    added viewing and layout. updated with turbolink for jquery, faster jquery loading.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery.turbolinks
 //= require_tree .
 //
 //= require bootstrap-sprockets
 //= require bootstrap
 //= require moment
 //= require bootstrap-datetimepicker
+
+jQuery(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.document.location = $(this).data("href");
+    });
+});
