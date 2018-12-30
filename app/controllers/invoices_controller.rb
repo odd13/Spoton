da27928df77a0f7561c4fd11ee75ba445b00c91f
Oddie <arcadeoddie@gmail.com>
2018-12-30 23:59:32 +1100

    Add the invoices controller.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,2 +1,51 @@
 class InvoicesController < ApplicationController
+  def index
+    @invoices = invoice.all
+  end
+
+  def new
+    @invoice = invoice.new
+  end
+
+  def create
+    @invoice = invoice.new(invoice_params)
+    if @invoice.save
+      redirect_to action: 'index'
+    else
+      redirect_to action: 'new'
+    end
+  end
+
+  def show
+    @invoice = invoice.find(params[:id])
+  end
+
+  def destroy
+    @invoice = invoice.find(params[:id])
+    @invoice.destroy
+    flash[:success] = 'invoice deleted'
+    redirect_to '/invoices'
+  end
+
+  def edit
+    @invoice = invoice.find(params[:id])
+  end
+
+  def update
+    @invoice = invoice.find(params[:id])
+    if @invoice.update(invoice_params)
+      @invoice.save
+      redirect_to action: :index
+    else
+      render 'invoices/edit'
+    end
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:work_type, :cust_name,
+                                    :cust_address, :price, :taxi,
+                                    :parent_id)
+  end
 end
